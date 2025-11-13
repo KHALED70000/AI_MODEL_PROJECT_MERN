53e02f0e74f829e52b7021ee7897ad69f7dca4a3
KHALED70000 <abdullah151337@gmail.com>
2025-11-13 15:37:48 +0600

    'Delete' model function added

--- src/PAGEs/MyModal/Mymodels.jsx
+++ src/PAGEs/MyModal/Mymodels.jsx
@@ -2,6 +2,10 @@ import React, { use, useEffect, useState } from 'react';
 import axiosInstance from '../../AXIOS_api/Axio';
 import { AuthContext } from '../../CONTEXT/AuthContext';
 import { NavLink } from 'react-router-dom';
+import { FaEdit } from 'react-icons/fa';
+import { RiDeleteBinLine } from 'react-icons/ri';
+import { toast } from 'react-toastify';
+import Swal from 'sweetalert2';
 
 const Mymodels = () => {
 
@@ -17,7 +21,58 @@ const Mymodels = () => {
             .catch((err) => console.error("Error fetching models:", err));
     }, [user]);
 
-    console.log(mymodels)
+    const handleDeleteModel = (_id) => {
+        Swal.fire({
+             title: "⚠️ Delete This Model?",
+    text: "You won’t be able to recover this model once deleted!",
+    icon: "warning",
+    background: "#0a0a0f",
+    color: "#e0f7ff",
+    iconColor: "#00ffff",
+    showCancelButton: true,
+    confirmButtonColor: "#00bfff",
+    cancelButtonColor: "#ff3366",
+    confirmButtonText: "Yes, Delete It!",
+    cancelButtonText: "Cancel",
+    customClass: {
+      popup: "rounded-3xl shadow-[0_0_25px_rgba(0,200,255,0.3)] border border-[#00bfff33]",
+      title: "text-neon-blue text-2xl font-semibold tracking-wide",
+      htmlContainer: "text-gray-300 text-sm font-light",
+      confirmButton:
+        "bg-[#00bfff] text-black font-semibold px-6 py-2 rounded-lg hover:shadow-[0_0_15px_#00ffff] transition-all duration-300 cursor-pointer",
+      cancelButton:
+        "bg-[#1e1e2f] text-gray-300 font-semibold px-6 py-2 rounded-lg hover:bg-[#2a2a3d] hover:text-white transition-all duration-300 ml-4 cursor-pointer",
+    },
+    buttonsStyling: false,
+    showClass: {
+      popup: "animate__animated animate__fadeInDown animate__faster",
+    },
+    hideClass: {
+      popup: "animate__animated animate__fadeOutUp animate__faster",
+    },
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosInstance.delete(`/Mymodels/${_id}`)
+                    .then(res => {
+                        console.log(res.data)
+                        const deltedModel = res.data;
+                        if (deltedModel.deletedCount > 0) {
+                            const remainingModels = mymodels.filter(model => model._id !== _id);
+                            setMymodels(remainingModels);
+                            Swal.fire({
+                                icon: "success",
+                                title: "The bid has been rejected successfully",
+                                showConfirmButton: false,
+                                timer: 1500
+                            });
+                        }
+                    })
+                    .catch()
+            }
+        })
+
+    }
+
     return (
         <div className='mt-30 container mx-auto'>
             <h1 className="text-3xl font-semibold text-center text-blue-400 mb-16">My AI Models</h1>
@@ -41,8 +96,8 @@ const Mymodels = () => {
                                 </p>
 
                                 <div className='flex justify-between gap-4'>
-                                    <NavLink to={`/viewmodel/${allmodel._id}`} className='w-full mt-4 text-center bg-neon-blue text-white font-semibold py-3 rounded-xl shadow-[0_0_10px_rgba(0,200,255,0.6)] hover:shadow-[0_0_15px_rgba(0,200,255,0.9)] transition-all duration-300 hover:scale-102'>View Details</NavLink>
-                                    <NavLink to={`/viewmodel/${allmodel._id}`} className='w-full mt-4 text-center bg-neon-blue text-white font-semibold py-3 rounded-xl shadow-[0_0_10px_rgba(0,200,255,0.6)] hover:shadow-[0_0_15px_rgba(0,200,255,0.9)] transition-all duration-300 hover:scale-102'>View Details</NavLink>
+                                    <button className='flex items-center justify-center gap-2 w-full mt-4 text-center bg-neon-blue text-white font-semibold py-3 rounded-xl shadow-[0_0_10px_rgba(0,200,255,0.6)] hover:shadow-[0_0_15px_rgba(0,200,255,0.9)] transition-all duration-300 hover:scale-102'>Edit <span className='text-blue-500'><FaEdit size={20} /></span></button>
+                                    <button onClick={() => handleDeleteModel(allmodel._id)} className='flex items-center justify-center gap-2 w-full mt-4 text-center bg-neon-blue text-white font-semibold py-3 rounded-xl shadow-[0_0_10px_rgba(0,200,255,0.6)] hover:shadow-[0_0_15px_rgba(0,200,255,0.9)] transition-all duration-300 hover:scale-102'>Delete <span className='text-red-600'><RiDeleteBinLine size={20} /></span> </button>
                                 </div>
                                 <NavLink to={`/viewmodel/${allmodel._id}`} className='mt-4 text-center bg-neon-blue text-white font-semibold py-3 rounded-xl shadow-[0_0_10px_rgba(0,200,255,0.6)] hover:shadow-[0_0_15px_rgba(0,200,255,0.9)] transition-all duration-300 hover:scale-102'>View Details</NavLink>
                             </div>
